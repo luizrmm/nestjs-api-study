f999059af5952a7c483f05494b709bdac160b3dc
Luiz <luizrmm97@gmail.com>
2020-02-14 14:35:12 -0300

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'ideas',
+      synchronize: true,
+      logging: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
